f16b21cb44db6523737c0fc49a61e2c81e39f10e
NIKHILCHAUHAN87667 <nikhilchauhan87667@gmail.com>
2025-12-04 18:06:38 +0530

    Add biomed search routes to app.js

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require('./config/db');
+const biomedRoutes = require("./biomedsearch");
 app.use(express.json());
 app.use(cors());
+
 
 // Routes
 app.use('/api/bundle', authMiddleware, require('./routes/bundleRoutes'));
@@ -14,6 +16,8 @@ app.use('/api/expand', authMiddleware, require('./routes/expand'));
 app.use('/api/translate', authMiddleware, require('./routes/translate'));
 app.use('/api/abha', require('./middleware/abhavalidator.js'));
 app.use('/api/report', authMiddleware, require('./routes/report'));
+
+app.use("/api/search-biomed-icd", biomedRoutes);
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
