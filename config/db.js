ff0b792a5b4076382f23abe1d36910f09cfc865e
mahos18 <sohamlohote@gmail.com>
2025-12-04 18:14:56 +0530

    bundl,audit db fx

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 
-const uri = 'mongodb+srv://devstatic014_db_user:zmkbeTqXgLMU3ocv@cluster0.xxcjgvv.mongodb.net/?appName=Cluster0/ayushsetu';
-mongoose.connect(uri)
+const uri =process.env.MONGO_URI ;
+const DB_NAME = "ayushsetu";
+mongoose.connect(uri,{
+  dbName: DB_NAME,
+})
   .then(() => {
-    console.log("Connected to MongoDB");
+    console.log("Connected to MongoDB :",DB_NAME);
   })
   .catch((error) => {
     console.error("Database connection failed:", error);
