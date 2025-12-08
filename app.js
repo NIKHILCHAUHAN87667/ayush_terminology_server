1eb6ba412f904a024e84a01fd9cde389ee98f353
NIKHILCHAUHAN87667 <nikhilchauhan87667@gmail.com>
2025-12-08 16:28:06 +0530

    routefix-wrapperfhir

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ app.use('/api/expand', authMiddleware, require('./routes/expand'));
 app.use('/api/translate', authMiddleware, require('./routes/translate'));
 app.use('/api/abha', require('./middleware/abhavalidator.js'));
 app.use('/api/report', authMiddleware, require('./routes/report'));
+app.use('/fhir',require('./routes/fhirExpandWrapper'));
+app.use('/fhir',require('./routes/fhirTranslateWrapper '));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
